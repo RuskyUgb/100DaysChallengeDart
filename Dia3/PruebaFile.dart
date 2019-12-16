cd8579e8e6978ca218fcfd4843f53eac5710a92e
ElGalactico <ugbcaballero@gmail.com>
2019-12-16 17:55:43 -0600

    Agregue algo del manejo de archivos

--- Dia3/PruebaFile.dart
+++ Dia3/PruebaFile.dart
@@ -1,10 +1,27 @@
 import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
+
 void main(){
     
     var NombreArchivo = "Prueba1.txt";
-    new File(NombreArchivo).writeAsString("Hola Dart");
-    new File(NombreArchivo).readAsString().then((String contents) {
-    print(contents);
-  });
+    var file = new File(NombreArchivo);
+    //new File(NombreArchivo).writeAsString("Hola Dart tílde");
+    //Es decir el then solicita una funcion, que aplique algo al documento
+    //Funcion anonima o cualquiera que tenga un parametro tipo String
+    new File(NombreArchivo).readAsString().then(mostrarTexto);
 
+    Stream<List<int>> inputStream = file.openRead();
+    inputStream
+      .transform(utf8.decoder)
+      .transform(new LineSplitter())
+      .listen(LecturaStream);
+}
+
+void mostrarTexto(String texto){
+  print("$texto : ${texto.length}");
+}
+
+void LecturaStream(String line){
+  print('$line: ${line.length} bytes');
 }
